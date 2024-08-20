801ddbb38d7c9a8cdc437c415df606c6cda3fe9f
Hem-1112 <hempatel1234@gmail.com>
2024-08-20 13:26:37 +0000

    Initial Commit

---
+++ backendcust/src/main/java/com/backendcust/BackendcustApplication.java
@@ -0,0 +1,13 @@
+package com.backendcust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendcustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendcustApplication.class, args);
+	}
+
+}
